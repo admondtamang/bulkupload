40999cfc891670d2903ac5eaf069711c78690eab
admond <admondtamang@gmail.com>
2022-07-04 22:58:33 +0545

    [complete] stage 1

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 // const fs = require("fs").promises;
 const path = require("path");
-const excludeFileTypes = ['.exe', '.DS_Store'];
-const fs = require('fs-extra');
-const { moveDirectory, printObject } = require('./utils');
+const excludeFileTypes = [".exe", ".DS_Store"];
+const fs = require("fs-extra");
+const { moveDirectory, printObject } = require("./utils");
 
-let result=[];
+let result = [];
 
+let attachments = [];
+let document = {};
+let count = 0;
 async function findFiles(folderName) {
   // list folders
-  let items = await fs.readdir(folderName, { withFileTypes: true });  
+  let items = await fs.readdir(folderName, { withFileTypes: true });
 
   // Browser folders
-  let attachments=[];
- const res= items.map((item,index) => {
-    let document = { name: folderName};
-    const filePath = path.join(folderName, item.name);;
+  await Promise.all(
+    items.map(async (item) => {
+      document = { name: folderName };
+      const filePath = await path.join(folderName, item.name);
 
-    // donot include file extension
-    if (excludeFileTypes.includes(item.name)) return; 
-    
-    // file found
-    if (path.extname(item.name) === '.json') {
-      attachments.push(filePath)
+      // donot include file extension
+      if (excludeFileTypes.includes(item.name)) return;
 
-      result[index] = { ...document, attachments}
-      // result.push({...document, attachments})
-      
-      console.log(`Found file: ${item.name} ${index} in folder: ${folderName}`);
-    } else {
-      // find next folder if finish finding files
-      // recursive function for other folder
-      attachments=[]
-      return findFiles(filePath);
-    }
-  })
+      // file found
+      if (path.extname(item.name) === ".json") {
+        attachments.push(filePath);
+        result[count] = { ...document, attachments };
 
-  printObject(attachments)
-return attachments  
+        console.log(
+          `Found file: ${filePath} ${count} in folder: ${folderName}`
+        );
+      } else {
+        // find next folder if finish finding files
+        await findFiles(filePath);
+      }
+    })
+  );
+
+  // reset parameters and count document
+
+  attachments = [];
+  document = {};
+  count = count + 1;
+  // moveDirectory('23121');
+  console.log("=============================");
+  console.log("Finish Document ", folderName, count);
+  console.log("=============================");
 }
 
-
-
-// moveDirectory('23121');
-//  findFiles('stores');
-
-
 (async () => {
-  try {  
-    const result = await findFiles('stores');
-    printObject("Final=",result)
+  try {
+    // find files of folder stores
+    await findFiles("stores");
   } catch (e) {
-    console.log("error",e);
+    console.log("error", e);
   }
 })();
